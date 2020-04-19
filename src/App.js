496e03bdd7ccef463d4882d753645191ed833c23
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-04-19 16:52:33 +0100

    fix(serviceWorker): remove serviceWorker file and imports

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import Tree from "./components/Tree/Tree";
 import "./fontawesome";
-import initialTree from "./mocks/initialTree";
+// import initialTree from "./mocks/initialTree";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tree initialTree={initialTree} />
+        {/* You can try it out by providing an initial tree.
+        As long as its shape is respected it should work seamlessly */}
+        <Tree
+        // initialTree={initialTree}
+        />
       </header>
     </div>
   );
